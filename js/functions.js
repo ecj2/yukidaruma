de037279829a055d76d4eff769f7446c449eacc7
Eric <magmoorian@gmail.com>
2025-11-08 05:46:06 -0600

    Move global functions to its own file

---
+++ js/functions.js
@@ -0,0 +1,66 @@
+"use strict";
+
+function setSeed(s) {
+
+  seed = s;
+}
+
+function getRandomNumber() {
+
+  // Use the so-called "xorshift algorithm" as described on https://en.wikipedia.org/wiki/Xorshift.
+
+  seed ^= seed << 13;
+  seed ^= seed >> 17;
+  seed ^= seed << 5;
+
+  let number = Math.abs(seed);
+
+  let number_of_digits = number.toString().length;
+
+  let divisor = "1";
+
+  let i = 0;
+
+  for (i; i < number_of_digits; ++i) {
+
+    divisor += "0";
+  }
+
+  // Return a pseudo-random number between 0.0 and 1.0.
+  return number / parseInt(divisor);
+}
+
+function getRandomRange(max) {
+
+  return (getRandomNumber() * 100 | 0) % max;
+}
+
+function isColliding(a_x, a_y, b_x, b_y, a_w, a_h, b_w, b_h) {
+
+  if (a_w == undefined || a_h == undefined || b_w == undefined || b_h == undefined) {
+
+    // Default to tile size for all dimensions.
+    a_w = TILE_SIZE;
+    a_h = TILE_SIZE;
+    b_w = TILE_SIZE;
+    b_h = TILE_SIZE;
+  }
+
+  return a_x + a_w > b_x && a_x < b_x + b_w && a_y + a_h > b_y && a_y < b_y + b_h;
+}
+
+function getDistance(a_x, a_y, b_x, b_y) {
+
+  // Move origins to centers.
+  a_x += TILE_SIZE / 2;
+  a_y += TILE_SIZE / 2;
+  b_x += TILE_SIZE / 2;
+  b_y += TILE_SIZE / 2;
+
+  return Math.sqrt(Math.pow(b_x - a_x, 2) + Math.pow(b_y - a_y, 2));
+}
+
+function convertDegreesToRadians(degrees) {
+
+  return degrees * Math.PI / 180;
+}
--- js/global.js
+++ js/global.js
@@ -35,38 +35,6 @@ const STATE_INTRO = 0;
 const STATE_GAME = 1;
 const STATE_DEFEAT = 2;
 
-// Functions.
-
-function isColliding(a_x, a_y, b_x, b_y, a_w, a_h, b_w, b_h) {
-
-  if (a_w == undefined || a_h == undefined || b_w == undefined || b_h == undefined) {
-
-    // Default to tile size for all dimensions.
-    a_w = TILE_SIZE;
-    a_h = TILE_SIZE;
-    b_w = TILE_SIZE;
-    b_h = TILE_SIZE;
-  }
-
-  return a_x + a_w > b_x && a_x < b_x + b_w && a_y + a_h > b_y && a_y < b_y + b_h;
-}
-
-function getDistance(a_x, a_y, b_x, b_y) {
-
-  // Move origins to centers.
-  a_x += TILE_SIZE / 2;
-  a_y += TILE_SIZE / 2;
-  b_x += TILE_SIZE / 2;
-  b_y += TILE_SIZE / 2;
-
-  return Math.sqrt(Math.pow(b_x - a_x, 2) + Math.pow(b_y - a_y, 2));
-}
-
-function convertDegreesToRadians(degrees) {
-
-  return degrees * Math.PI / 180;
-}
-
 // Miscellaneous.
 let animation_ticks = 0;
 let animation_frame = 0;
@@ -91,31 +59,6 @@ let move_background = false;
 // This can't be 0.
 let seed = 1;
 
-function getRandomNumber() {
-
-  // Use the so-called "xorshift algorithm" as described on https://en.wikipedia.org/wiki/Xorshift.
-
-  seed ^= seed << 13;
-  seed ^= seed >> 17;
-  seed ^= seed << 5;
-
-  let number = Math.abs(seed);
-
-  let number_of_digits = number.toString().length;
-
-  let divisor = "1";
-
-  let i = 0;
-
-  for (i; i < number_of_digits; ++i) {
-
-    divisor += "0";
-  }
-
-  // Return a pseudo-random number between 0.0 and 1.0.
-  return number / parseInt(divisor);
-}
-
 let birds_x = CANVAS_W + TILE_SIZE;
 let birds_y = 0;
 
@@ -137,17 +80,7 @@ let hit_special = false;
 
 let master_gain = 0.75;
 
-function setSeed(s) {
-
-  seed = s;
-}
-
 let music_speed = 1.0;
-
-function getRandomRange(max) {
-
-  return (getRandomNumber() * 100 | 0) % max;
-}
 
 let goal;
 let goals_met;
